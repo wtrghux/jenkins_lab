9eb74476c9adf5aa5bad6b6e8993c696d17f82fd
『   』 <59111565+wtrghux@users.noreply.github.com>
2021-03-04 19:37:09 +0500

    change pytest file

--- test_main.py
+++ test_main.py
@@ -1,36 +1,18 @@
 import os
-import main
+import pytest
 from peewee import *
+from main import init, fill, Clients, Orders
 
 
 DB_NAME = "Database.db"
-db = SqliteDatabase(DB_NAME)
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class Clients(BaseModel):
-    name = CharField()
-    city = CharField()
-    address = CharField()
-
-
-class Orders(BaseModel):
-    client = ForeignKeyField(Clients)
-    date = DateField()
-    amount = CharField()
-    description = CharField()
 
 
 def check_db_exists():
     if os.path.exists(DB_NAME):
         os.remove(DB_NAME)
-    main.init()
+    init()
     if os.path.exists(DB_NAME):
-        main.fill()
+        fill()
         return True
     else:
         return False
